f6a6ba0b6de339836cb3bea7b64365e87d3ac208
MaxPC <jhosh1212@gmail.com>
2022-10-13 23:55:04 +0700

    I have problems

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+// Stub
+public class PostRepository {
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicInteger size = new AtomicInteger(0);
+
+    public List<Post> all() {
+        return posts.values()
+                .stream()
+                .filter(x -> !x.isRemoved())
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Post> getById(long id) {
+       var post = posts.get(id);
+        return post == null || post.isRemoved() ? Optional.empty() : Optional.of(post);
+    }
+
+    public Post save(Post post) {
+        if(post.getId() == 0){
+            post.setId(size.addAndGet(1));
+        }else {
+            var found = getById(post.getId()).orElseThrow(NotFoundException::new);
+            if (found.isRemoved()) throw new NotFoundException();
+        }
+        posts.put(post.getId(),post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        Post post =getById(id).orElseThrow(NotFoundException::new);
+        post.setRemoved(true);
+    }
+}
---
+++ servlets/src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all()
+                .stream()
+                .filter(x -> !x.isRemoved())
+                .collect(Collectors.toList());
+    }
+
+    public Post getById(long id) {
+        var post = repository.getById(id).orElseThrow(NotFoundException::new);
+        if (post.isRemoved()) throw new NotFoundException();
+        return post;
+    }
+
+    public Post save(Post post) {
+        if (post.getId() != 0) {
+            var found = repository.getById(post.getId()).orElseThrow(NotFoundException::new);
+            if (found.isRemoved()) throw new NotFoundException();
+        }
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        var post = repository.getById(id).orElseThrow(NotFoundException::new);
+        if (post.isRemoved()) throw new NotFoundException();
+        repository.removeById(id);
+    }
+}
+
