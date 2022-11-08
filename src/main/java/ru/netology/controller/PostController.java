9d601d21da317c1fb481b45c69a232bd1aea3ed1
MaxPC <jhosh1212@gmail.com>
2022-11-08 19:42:46 +0700

    di_annotation_v2

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,7 +1,7 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
+@Controller
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
@@ -30,6 +31,8 @@ public class PostController {
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
+
+
     final var gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
     final var post = gson.fromJson(body, Post.class);
     final var data = service.save(post);
