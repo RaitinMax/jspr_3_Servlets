010e4b9c06bc25220c0c499607d351ce1a139d91
MaxPC <jhosh1212@gmail.com>
2022-11-23 20:08:31 +0700

    SpringMVC

---
+++ src/main/java/ru/netology/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ru.netology.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+@Configuration
+public class AppConfig {
+    @Bean
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+        final var bean = new RequestMappingHandlerAdapter();
+        bean.getMessageConverters().add(new GsonHttpMessageConverter());
+        return bean;
+    }
+}
